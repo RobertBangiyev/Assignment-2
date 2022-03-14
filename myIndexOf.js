afebf36e7f68f0e59e21f76520b2953f5f5da6c0
Mohammad Sarker <44179446+Mohammad-Sarker@users.noreply.github.com>
2022-03-14 13:31:48 -0400

    updated function return to correct index

--- myIndexOf.js
+++ myIndexOf.js
@@ -1,15 +1,13 @@
-Array.prototype.indexOf = (array, targetElement) => {
+Array.prototype.myIndexOf = (array, targetElement) => {
     let output = -1;
     for (index = 0; index < array.length; index++) {
-        var arrayvalue = array[index];
-
-        if (arrayvalue == targetElement) {
-            output = index;
+        if (array[index] == targetElement) {
+            return index;
         }
     }
     return output;
 }
 
 //Test
-indexTest = Array.prototype.indexOf([6, 98, 54, 8, 11], 11);
-console.log(indexTest);
+samplearray = [6, 98, 54, 8, 11, 11]
+indexTest = Array.prototype.myIndexOf(samplearray,11);
