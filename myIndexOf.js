2f4ce8a195372a56f07f17aa4946258851f68002
Mohammad Sarker <sarkerm948@gmail.com>
2022-03-14 13:30:18 -0400

    updated function return

--- myIndexOf.js
+++ myIndexOf.js
@@ -9,7 +9,5 @@ Array.prototype.myIndexOf = (array, targetElement) => {
 }
 
 //Test
-samplearray = [6, 98, 54, 8, 8, 11, 11]
-indexTest = Array.prototype.myIndexOf(samplearray, 8);
-console.log(indexTest);
-console.log(samplearray.indexOf(8));
+samplearray = [6, 98, 54, 8, 11, 11]
+indexTest = Array.prototype.myIndexOf(samplearray,11);
