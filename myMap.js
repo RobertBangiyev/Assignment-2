4dbf37b4287978061f7c748bb0360d8a8909c0ad
Robert Bangiyev <rbangiyev123@gmail.com>
2022-03-14 13:10:00 -0400

    Completed myMap function, tested it using two separate arrays and functions

---
+++ myMap.js
@@ -0,0 +1,23 @@
+Array.prototype.myMap = function(callback) {
+    let output = [];
+    for(let i = 0; i < this.length;i++) {
+      output.push(callback(this[i]));
+    }
+    return output;
+}
+
+const array1 = [1, 4, 9, 16];
+
+const map1 = array1.map(x => x * 2);
+const map2 = array1.myMap(x => x * 2)
+
+console.log(map1); //prints [2, 8, 18, 32]
+console.log(map2); //prints [2, 8, 18, 32]
+
+const anotherArray = [1, 3, 5, 7];
+
+const anotherMap1 = anotherArray.map(x => x + 1);
+const anotherMap2 = anotherArray.myMap(x => x + 1);
+
+console.log(anotherMap1); //prints [2, 4, 6, 8]
+console.log(anotherMap2); //prints [2, 4, 6, 8]
