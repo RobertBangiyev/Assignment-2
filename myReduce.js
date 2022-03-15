bce41bb1589843b2884ede343917b8689104a6ba
Robert Bangiyev <rbangiyev123@gmail.com>
2022-03-15 19:19:50 -0400

    Updated to work for case where element in array is empty

--- myReduce.js
+++ myReduce.js
@@ -1,12 +1,16 @@
 Array.prototype.myReduce = function(callback, startValue) {
     let output = startValue;
     for(let i = 0; i < this.length; i++) {
+        if(this[i] === undefined) {
+            continue;
+        }
         output = callback(output, this[i]);
     }
     return output;
 }
 
 const arr = [1, 2, 3, 4];
+let myArray = [1,2,,4,5];
 
 const startVal = 0;
 const sum = arr.reduce(
@@ -22,3 +26,18 @@ const mySum = arr.myReduce(
 );
 
 console.log(mySum); //prints 10
+
+const newSum = myArray.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    startVal
+);
+
+console.log(newSum); //prints 12
+
+const newerSum = myArray.myReduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    startVal
+);
+
+console.log(newerSum); //prints 12
+
