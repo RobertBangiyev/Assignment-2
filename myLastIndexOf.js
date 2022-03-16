6af22397e211bcf4afb5a1cf638c77498b1f7af1
Mohammad Sarker <sarkerm948@gmail.com>
2022-03-15 21:25:00 -0400

    updated function with fromIndex and -1 return for negative index

--- myLastIndexOf.js
+++ myLastIndexOf.js
@@ -1,13 +1,32 @@
-Array.prototype.lastindexOf = (array, targetElement) => {
+Array.prototype.myLastIndexOf = function (targetElement, fromIndex = this.length - 1) {
     let output = -1;
-    for (index = 0; index < array.length; index++) {
-        if (array[index] == targetElement) {
-            output = index;
+    let searchIndex = fromIndex;
+    if(searchIndex < 0) {
+        searchIndex = this.length + searchIndex;
+    }
+    if(searchIndex < 0) {
+        return output;
+    }
+    for (let i = searchIndex; i >= 0; i--)  {
+        if (this[i] == targetElement) {
+            return i;
         }
     }
     return output;
-}
+};
+
 
 //Test
-LastindexTest = Array.prototype.lastindexOf([108, 18, 8, 8, 9, 9], 9);
-console.log(LastindexTest);
+const numbers = [2, 5, 9, 2];
+
+console.log(numbers.lastIndexOf(2, 3));
+console.log(numbers.myLastIndexOf(2, 3));
+
+console.log(numbers.lastIndexOf(2, 2));
+console.log(numbers.myLastIndexOf(2, 2));
+
+console.log(numbers.lastIndexOf(2, -2));
+console.log(numbers.myLastIndexOf(2, -2));
+
+console.log(numbers.lastIndexOf(2, -1));
+console.log(numbers.myLastIndexOf(2, -1));
